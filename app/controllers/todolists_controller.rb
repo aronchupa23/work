57eaaeda0d3355e8e1e302108f12527954a73525
aronchupa23 <naoki1194_boowy23@icloud.com>
2020-02-07 10:47:40 +0900

    [finish] 3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,12 +5,16 @@ class TodolistsController < ApplicationController
     end
     # 以下を追加
     def create
-        # ストロングパラメーターを使用
-         list = List.new(list_params)
-        # DBへ保存する
-         list.save
-        # トップ画面へリダイレクト
-        redirect_to '/top'
+        list = List.new(list_params)
+        list.save # データベースへ保存する
+
+        redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
+    end
+    def index
+          @lists = List.all
+    end
+    def show
+      @list = List.find(params[:id])
     end
     private
 
