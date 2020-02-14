f01d3e9d83df041a5753515626b1a6ec4398fd6a
aronchupa23 <naoki1194_boowy23@icloud.com>
2020-02-14 17:12:18 +0900

    [finish] 3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,34 +1,35 @@
 class TodolistsController < ApplicationController
 
   def new
-         @list = List.new
-    end
-    # 以下を追加
-    def create
-        list = List.new(list_params)
-        list.save # データベースへ保存する
-        redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
-    end
-    def index
-          @lists = List.all
-    end
-    def show
-      @list = List.find(params[:id])
-    end
+    @list = List.new
+  end
 
-    def edit
-      @list = List.find(params[:id])
-    end
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to todolist_path(list.id)
+  end
 
-    def update
-      list = List.find(params[:id])
-        list.update(list_params)
-        redirect_to todolist_path(list.id)
-    end
+  def index
+    @lists = List.all
+  end
 
-    private
+  def show
+    @list = List.find(params[:id])
+  end
 
-    def list_params
-        params.require(:list).permit(:title, :body, :image)
-    end
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_111949) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "imge_id"
+    t.string "image_id"
   end
 
 end
